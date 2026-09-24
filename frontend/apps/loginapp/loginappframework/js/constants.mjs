5890f445239eebf6fb4392e0f33157e98423fc87
nishant-bin <nishant.bindhani@deeplogictech.com>
2026-09-24 15:53:39 +0530

    Add txtc param to fix hardcoded white text/logo on loginapp pages against light bgc backgrounds

--- frontend/apps/loginapp/loginappframework/js/constants.mjs
+++ frontend/apps/loginapp/loginappframework/js/constants.mjs
@@ -70,6 +70,7 @@ export const APP_CONSTANTS = {
     GOOGLE_CLIENT_ID:"29460972280-iij9ke0rv6qu2r67u9f5co81h5kh4tp8.apps.googleusercontent.com",//GoogleSSO client Id goes here
 
     "DEFAULT_BGC": "#4FB4ED",
+    "DEFAULT_TXTC": "#ffffff",
 
     USER_ROLE: "user",
     GUEST_ROLE: "guest",
